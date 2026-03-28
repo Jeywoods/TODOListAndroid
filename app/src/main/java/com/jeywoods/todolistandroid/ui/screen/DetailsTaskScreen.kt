873b0a7fb4cde0a5cbba30998c3954979227a5c6
Jeywoods <VladSagaydak1@mail.ru>
2026-03-28 10:47:00 +0300

    deleted unused directives

--- app/src/main/java/com/jeywoods/todolistandroid/ui/screen/DetailsTaskScreen.kt
+++ app/src/main/java/com/jeywoods/todolistandroid/ui/screen/DetailsTaskScreen.kt
@@ -20,12 +20,8 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Snackbar
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
